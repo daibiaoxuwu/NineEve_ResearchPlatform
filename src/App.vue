fd60aaaad9e0f73f428a0513068f49ca39e1b06a
杜迦罗 <dujl16@mails.tsinghua.edu.cn>
2018-10-03 10:24:56 +0800

    added other objects
    seems that grid system is simply not beautifying itseif...

--- src/App.vue
+++ src/App.vue
@@ -96,7 +96,29 @@
       </div>
     </div>
   </div>
+   <b-list-group>
+  <b-list-group-item>Cras justo odio</b-list-group-item>
+  <b-list-group-item>Dapibus ac facilisis in</b-list-group-item>
+  <b-list-group-item>Morbi leo risus</b-list-group-item>
+  <b-list-group-item>Porta ac consectetur ac</b-list-group-item>
+  <b-list-group-item>Vestibulum at eros</b-list-group-item>
+</b-list-group>
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
    </div>
+
 </template>
 
 <script>
