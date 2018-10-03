d0a0a2c78955889934b3183c9ccdb282068056e5
杜迦罗 <dujl16@mails.tsinghua.edu.cn>
2018-10-03 12:42:48 +0800

    add login...bug with dropdown

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,5 +1,176 @@
 <template>
-    <div> login page </div>
+    <div>  
+        <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="text-center col-md-7 mx-auto"> <i class="fa d-block fa-bullseye fa-5x mb-4 text-info"></i>
+          <h2>导师信息填写</h2>
+          <p class="lead">tutor's information form</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 order-md-2">
+          <h4 class="d-flex justify-content-between mb-3"> <span class="text-muted"><b>Your cart</b></span> <span class="badge badge-secondary badge-pill">3</span> </h4>
+          <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between">
+              <div>
+                <h6 class="my-0"><b>Product name</b></h6> <small class="text-muted">Brief description</small>
+              </div> <span class="text-muted">$12</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between">
+              <div>
+                <h6 class="my-0"><b>Second product</b></h6> <small class="text-muted">Brief description</small>
+              </div> <span class="text-muted">$8</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between">
+              <div>
+                <h6 class="my-0"><b>Third item</b></h6> <small class="text-muted">Brief description</small>
+              </div> <span class="text-muted">$5</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between bg-light">
+              <div class="text-success">
+                <h6 class="my-0"><b>Promo code</b></h6> <small>EXAMPLECODE</small>
+              </div> <span class="text-success">-$5</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between"> <span>Total (USD)</span> <b>$20</b> </li>
+          </ul>
+          <form class="card p-2 my-4">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Promo code">
+              <div class="input-group-append">
+                <button type="submit" class="btn btn-secondary">Redeem</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="col-md-8 order-md-1">
+          <h4 class="mb-3"><b>Basic Information 基本信息</b></h4>
+          <form class="needs-validation" novalidate="">
+            <div class="row">
+              <div class="col-md-6 mb-3"> <label for="firstName">Last name 姓<br></label>
+                <input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
+                <div class="invalid-feedback"> Valid first name is required. </div>
+              </div>
+              <div class="col-md-6 mb-3"> <label for="lastName">First name 名</label>
+                <input type="text" class="form-control" id="lastName" placeholder="" value="" required="">
+                <div class="invalid-feedback"> Valid last name is required. </div>
+              </div>
+            </div>
+            <div class="mb-3"> <label for="username">Username</label>
+              <div class="input-group">
+                <div class="input-group-prepend"> <span class="input-group-text">@</span> </div>
+                <input type="text" class="form-control" id="username" placeholder="Username" required="">
+                <div class="invalid-feedback" style="width: 100%;"> Your username is required. </div>
+              </div>
+            </div>
+            <div class="mb-3"> <label for="email">Tutor's ID 教师号<br></label>
+              <input type="" class="form-control" id="email">
+              <div class="invalid-feedback"> Please enter a valid email address for shipping updates. </div>
+            </div>
+            <div class="mb-3"> <label for="email">Laboratory 研究所<br></label>
+              <div class="mb-3">
+                <button class="form-control btn btn-outline-primary dropdown-toggle" data-toggle="dropdown"> Dropdown </button>
+                <div class="dropdown-menu"> <a class="dropdown-item" href="#">Action</a>
+                  <div class="dropdown-divider"></div> <a class="dropdown-item" href="#">Separated link</a>
+                </div>
+              </div>
+              <div class="invalid-feedback"> Please enter a valid email address for shipping updates. </div>
+            </div>
+            <div class="mb-3"> <label for="email">Email <span class="text-muted">(Optional)</span></label>
+              <input type="email" class="form-control" id="email" placeholder="you@example.com">
+              <div class="invalid-feedback"> Please enter a valid email address for shipping updates. </div>
+            </div>
+            <div class="mb-3"> <label for="address">Address</label>
+              <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
+              <div class="invalid-feedback"> Please enter your shipping address. </div>
+            </div>
+            <div class="mb-3"> <label for="address2">Address 2 <span class="text-muted">(Optional)</span></label>
+              <input type="text" class="form-control" id="address2" placeholder="Apartment or suite"> </div>
+            <div class="row">
+              <div class="col-md-5 mb-3"> <label for="country">Country</label> <select class="custom-select d-block w-100" id="country" required="">
+                  <option value="">Choose...</option>
+                  <option value="United States">United States</option>
+                </select>
+                <div class="invalid-feedback"> Please select a valid country. </div>
+              </div>
+              <div class="col-md-4 mb-3"> <label for="state">State</label> <select class="custom-select d-block w-100" id="state" required="">
+                  <option value="">Choose...</option>
+                  <option value="California">California</option>
+                </select>
+                <div class="invalid-feedback"> Please provide a valid state. </div>
+              </div>
+              <div class="col-md-3 mb-3"> <label for="zip">Zip</label>
+                <input type="text" class="form-control" id="zip" placeholder="" required="">
+                <div class="invalid-feedback"> Zip code required. </div>
+              </div>
+            </div>
+            <hr class="mb-4">
+            <div class="custom-control custom-checkbox">
+              <input type="checkbox" class="custom-control-input" id="same-address" value="on"> <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label> </div>
+            <div class="custom-control custom-checkbox">
+              <input type="checkbox" class="custom-control-input" id="save-info" value="on"> <label class="custom-control-label" for="save-info">Save this information for next time</label> </div>
+            <hr class="mb-4">
+            <h4 class="mb-3"><b>Payment</b></h4>
+            <div class="d-block my-3">
+              <div class="custom-control custom-radio">
+                <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked="" required="" value="on"> <label class="custom-control-label" for="credit">Credit card</label> </div>
+              <div class="custom-control custom-radio">
+                <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required="" value="on"> <label class="custom-control-label" for="debit">Debit card</label> </div>
+              <div class="custom-control custom-radio">
+                <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required="" value="on"> <label class="custom-control-label" for="paypal">Paypal</label> </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6 mb-3"> <label for="cc-name">Name on card</label>
+                <input type="text" class="form-control" id="cc-name" placeholder="" required=""> <small class="text-muted">Full name as displayed on card</small>
+                <div class="invalid-feedback"> Name on card is required </div>
+              </div>
+              <div class="col-md-6 mb-3"> <label for="cc-number">Credit card number</label>
+                <input type="text" class="form-control" id="cc-number" placeholder="" required="">
+                <div class="invalid-feedback"> Credit card number is required </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-3 mb-3"> <label for="cc-expiration">Expiration</label>
+                <input type="text" class="form-control" id="cc-expiration" placeholder="" required="">
+                <div class="invalid-feedback"> Expiration date required </div>
+              </div>
+              <div class="col-md-3 mb-3"> <label for="cc-expiration">CVV</label>
+                <input type="text" class="form-control" id="cc-cvv" placeholder="" required="">
+                <div class="invalid-feedback"> Security code required </div>
+              </div>
+            </div>
+            <hr class="mb-4">
+            <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 text-muted text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 my-4">
+          <p class="mb-1">© 2017-2018 Company Name</p>
+          <ul class="list-inline">
+            <li class="list-inline-item">
+              <a href="#">Privacy</a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">Terms</a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">Support</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+     </div>
+     </div>
 </template>
 
 <style>
