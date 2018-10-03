4307af8d677a71c114dc0a49f13a61e12e11a68d
杜迦罗 <dujl16@mails.tsinghua.edu.cn>
2018-10-03 10:13:59 +0800

    add pingendo into project
    external css; only css is enough!

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,102 @@
 <template>
   <div id="app">
-    <div>
-    <b-alert show>Default Alert</b-alert>
-
-    <b-alert variant="success" show>Success Alert</b-alert>
-
-    <b-alert variant="danger"
-             dismissible
-             :show="showDismissibleAlert"
-             @dismissed="showDismissibleAlert=false">
-      Dismissible Alert!
-    </b-alert>
-
-    <b-alert :show="dismissCountDown"
-             dismissible
-             variant="warning"
-             @dismissed="dismissCountDown=0"
-             @dismiss-count-down="countDownChanged">
-      <p>This alert will dismiss after {{dismissCountDown}} seconds...</p>
-      <b-progress variant="warning"
-                  :max="dismissSecs"
-                  :value="dismissCountDown"
-                  height="4px">
-      </b-progress>
-    </b-alert>
-
-    <b-btn @click="showAlert" variant="info" class="m-1">
-      Show alert with count-down timer
-    </b-btn>
-    <b-btn @click="showDismissibleAlert=true" variant="info" class="m-1">
-      Show dismissible alert ({{showDismissibleAlert?'visible':'hidden'}})
-    </b-btn>
+ <nav class="navbar navbar-expand-md bg-primary navbar-dark">
+    <div class="container"> <a class="navbar-brand" href="#">Navbar</a> <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent"> <span class="navbar-toggler-icon"></span> </button>
+      <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item"> <a class="nav-link" href="#"><i class="fa d-inline fa-lg fa-bookmark-o"></i> Bookmarks</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#"><i class="fa d-inline fa-lg fa-envelope-o"></i> Contacts</a> </li>
+        </ul> <a class="btn navbar-btn btn-primary ml-2 text-white"><i class="fa d-inline fa-lg fa-user-circle-o"></i> Sign in</a>
+      </div>
+    </div>
+  </nav>
+  <div class="py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12" style="">
+          <div class="card">
+            <div class="card-header">PROJECT INFORMATION</div>
+            <div class="card-body">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>PROJECT NAME</th>
+                    <th>TEACHER</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>项目1</td>
+                    <td>Otto</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>项目2</td>
+                    <td>Thornton</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>项目3</td>
+                    <td>the Bird</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <ul class="pagination">
+            <li class="page-item"> <a class="page-link" href="#"> <span>«</span> <span class="sr-only">Previous</span> </a> </li>
+            <li class="page-item"> <a class="page-link" href="#">1</a> </li>
+            <li class="page-item"> <a class="page-link" href="#">2</a> </li>
+            <li class="page-item"> <a class="page-link" href="#">3</a> </li>
+            <li class="page-item"> <a class="page-link" href="#">4</a> </li>
+            <li class="page-item"> <a class="page-link" href="#"> <span>»</span> <span class="sr-only">Next</span> </a> </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
+  <div class="p-0">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header"> MY PROJECTS</div>
+            <div class="card-body">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>PROJECT NAME</th>
+                    <th>STATUS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>项目1</td>
+                    <td>Passed</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>项目2</td>
+                    <td>Rejected</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>项目3</td>
+                    <td>Rejected</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+   </div>
 </template>
 
 <script>
@@ -59,4 +123,3 @@ export default {
 <style lang="css">
 @import '../src/assets/theme.css'
 </style>
-
