4e0843f2079db745faac14834f26d3eebd1b29e4
杜迦罗 <dujl16@mails.tsinghua.edu.cn>
2018-10-22 16:01:20 +0800

    fixbug: studentInfo id and email
    #33

--- backend_simulator/bs_fake.js
+++ backend_simulator/bs_fake.js
@@ -30,7 +30,7 @@ app.use(appSession);
 
 app.get('(/|/register|/enroll)', (req, res) =>{
     var user = {};
-    if (req.session && req.session.user) { user.name = req.session.user; }
+    if (req.session && req.session.user) { user.name = req.session.user; }//TODO: what's this?
     res.render('index', {"user":JSON.stringify(user)});
 })
 
@@ -49,28 +49,30 @@ var requireLoc = "./pages_fake"; //location for requiring js files for database 
 
 //home.js
 var home = require(requireLoc+ "/home");
+var teacherInfo = require(requireLoc + "/teacherInfo");
+var studentInfo = require(requireLoc + "/studentInfo");
+var main = require(requireLoc + "/main");
+var enroll = require(requireLoc + "/enroll");
+
 
 app.get('/login/byEmail', function(sReq, sRes){
 	
 	home.emailLogin(sReq.query.email, sReq.query.password, function(result){
-        sReq.session.user = {name: sReq.query.email};
+        sReq.session.user = {id:"", email: sReq.query.email};
 		sRes.send(result);
 	});
-	
-	
-    //sRes.send(home.emailLogin(sReq.query.email, sReq.query.password));
 });
 
 app.get('/login/byTeacherId', function(sReq, sRes){
 	home.teacherLogin(sReq.query.teacherId, sReq.query.password,function(result){
-        sReq.session.user = {name: sReq.query.teacherId};
+        sReq.session.user = {id: sReq.query.teacherId, email:""};
 		sRes.send(result);
 	});
 });
 
 app.get('/login/byStudentId', function(sReq, sRes){
     home.studentLogin(sReq.query.studentId, sReq.query.password,function(result){
-        sReq.session.user = {name: sReq.query.studentId}   
+        sReq.session.user = {id: sReq.query.studentId, email:""}   
 		sRes.send(result);
 	});
 });
@@ -78,51 +80,47 @@ app.get('/login/byStudentId', function(sReq, sRes){
 app.get('/register/getUrl', function(sReq, sRes){
 	console.log(sReq.query);
    home.register(sReq.query.name,sReq.query.university,sReq.query.email,sReq.query.password,function(result){
-        sReq.session.user = {name: sReq.query.email}    //设置"全局变量"name. 此后可以根据这个区分用户.
+        sReq.session.user = {id:"", email: sReq.query.email}    //设置"全局变量"name. 此后可以根据这个区分用户.
 		sRes.send(result);
 	});
 });
 
-var teacherInfo = require(requireLoc + "/teacherInfo");
-var studentInfo = require(requireLoc + "/studentInfo");
-var main = require(requireLoc + "/main");
-var enroll = require(requireLoc + "/enroll");
 
 app.get('/teacherInfo/save', function(sReq, sRes) {
     console.log(sReq);
     console.log(sReq.query.lastName);
-    sRes.send(teacherInfo.teacherInfoSave(sReq.query.lastName, sReq.query.firstName, sReq.query.username,
+    sRes.send(teacherInfo.teacherInfoSave(sReq.session.user.id, sReq.query.lastName, sReq.query.firstName, sReq.query.username,
         sReq.query.wechatPhone, sReq.query.email, sReq.query.perWebAddr,
          sReq.query.researchArea, sReq.query.researchResults, sReq.query.lab));
 });
 
 app.get('/teacherInfo/launch', function(sReq, sRes) {
-    sRes.send(teacherInfo.teacherInfoLaunch(sReq.query.lastName, sReq.query.firstName, sReq.query.username,
+    sRes.send(teacherInfo.teacherInfoLaunch(sReq.session.user.id, sReq.query.lastName, sReq.query.firstName, sReq.query.username,
         sReq.query.wechatPhone, sReq.query.email, sReq.query.perWebAddr,
          sReq.query.researchArea, sReq.query.researchResults, sReq.query.lab));
 });
 
 app.get('/teacherInfo/get', function(sReq, sRes) {
-    sRes.send(teacherInfo.teacherInfoGet(sReq.session.user.name));
+    sRes.send(teacherInfo.teacherInfoGet(sReq.session.user.id, sReq.session.user.email));
 });
 
 
 app.get('/studentInfo/save', function(sReq, sRes) {
     console.log(sReq);
     console.log(sReq.query.lastName);
-    sRes.send(studentInfo.studentInfoSave(sReq.query.lastName, sReq.query.firstName, sReq.query.username,
+    sRes.send(studentInfo.studentInfoSave(sReq.session.user.id, sReq.session.user.email, sReq.query.lastName, sReq.query.firstName, sReq.query.username,
         sReq.query.wechatPhone, sReq.query.email, sReq.query.perWebAddr,
          sReq.query.breIntr, sReq.query.grade, sReq.query.selectedLab, sReq.query.selectedKey));
 });
 
 app.get('/studentInfo/launch', function(sReq, sRes) {
-    sRes.send(studentInfo.studentInfoLaunch(sReq.query.lastName, sReq.query.firstName, sReq.query.username,
+    sRes.send(studentInfo.studentInfoLaunch(sReq.session.user.id, sReq.session.user.email, sReq.query.lastName, sReq.query.firstName, sReq.query.username,
         sReq.query.wechatPhone, sReq.query.email, sReq.query.perWebAddr,
          sReq.query.breIntr, sReq.query.grade, sReq.query.selectedLab, sReq.query.selectedKey));
 });
 
 app.get('/studentInfo/get', function(sReq, sRes) {
-    sRes.send(studentInfo.studentInfoGet(sReq.session.user.name));
+    sRes.send(studentInfo.studentInfoGet(sReq.session.user.id, sReq.session.user.email));
 });
 
 
