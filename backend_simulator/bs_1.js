2cee9cd4486657e80d85b3d89ca31665e23cf508
黄冰鉴 <2016011296@secoder.net>
2018-11-01 15:22:07 +0800

    Add: add backend captcha interface
    issue #62

--- backend_simulator/bs_1.js
+++ backend_simulator/bs_1.js
@@ -77,6 +77,28 @@ var assignmentForm = require(requireLoc + "/assignmentForm");
 var right = require(requireLoc + "/right");
 var evaluate = require(requireLoc + "/evaluate");
 
+app.get('/api/getCaptcha', function(req, res) {
+    var captcha = svgCaptcha.create({  
+        // 翻转颜色  
+        inverse: false,  
+        // 字体大小  
+        fontSize: 36,  
+        // 噪声线条数  
+        noise: 2,  
+        // 宽度  
+        width: 80,  
+        // 高度  
+        height: 30,  
+    });  
+    // 保存到session,忽略大小写  
+    req.session = captcha.text.toLowerCase(); 
+    console.log(req.session); //0xtg 生成的验证码
+    //保存到cookie 方便前端调用验证
+    res.cookie('captcha', req.session); 
+    res.setHeader('Content-Type', 'image/svg+xml');
+    res.write(String(captcha.data));
+    res.end();
+})
 
 app.get('/login/byEmail', function(sReq, sRes){
   if (sReq.query.email.length<200 && sReq.query.password.length<200) {
