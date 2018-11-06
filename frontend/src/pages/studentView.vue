cde76f41750b3a7e25e77bbdb1a0b7cc143893af
国雨晴 <regenmelodie@163.com>
2018-11-06 23:27:49 +0800

    add: a part of studentView
    #77

---
+++ frontend/src/pages/studentView.vue
@@ -0,0 +1,18 @@
+
+<template>
+
+    <div>  
+    <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="text-center col-md-7 mx-auto"> <i class="fa d-block fa-bullseye fa-5x mb-4 text-student"></i>
+          <h2>学生信息</h2>
+          <p class="lead">Student studentrmation</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="">
+    <div class="container">
+      <div class="row">
+  <rightpane></rightpane>
