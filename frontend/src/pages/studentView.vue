eb993fc6511e54ccf210dbcc371e43a732b168a4
国雨晴 <regenmelodie@163.com>
2018-11-07 14:17:55 +0800

    add: studentView
    #77

--- frontend/src/pages/studentView.vue
+++ frontend/src/pages/studentView.vue
@@ -16,3 +16,78 @@
     <div class="container">
       <div class="row">
   <rightpane></rightpane>
+        
+        <div class="col-md-8 order-md-1">
+      
+
+         <h4 class="mb-3"><b>{{studentName}}</b>
+           <small class="form-text text-muted">
+             {{dropdownText}}
+              </small>
+         </h4>
+          
+            <b-pagination-nav base-url="#" :number-of-pages="num" v-model="currentPage" />
+
+
+   
+    <p class="form-text" style="font-weight:bold;">Email 邮箱</p>
+         <p class="form-text text-muted">
+           {{email}}</p>
+         <p class="form-text" style="font-weight:bold;">Personal Webpage Address 个人主页地址</p>
+         <p class="form-text text-muted">
+          {{perWebAddr}}
+</p>
+
+
+
+             <button class="btn btn-secondary btn-lg btn-block" @click="$router.go(-1)">Home 返回主页</button>
+        
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  
+     </div>
+</template>
+
+
+<script>
+import rightpane from '../components/right.vue';
+
+export default {
+  data() {
+    return {
+      dropdownText: 'Software Laboratory 软件所',
+      grade: "Junior 大三",
+      studentName:"学生名",
+      email:"1@1",
+      perWebAddr:"1.com",
+      breIntr:"个人简介",
+
+      visible: 'invisible',
+      isTeacher: false,
+      currentPage:1,
+      num:1
+    };
+  },
+  components: {
+    rightpane
+  },
+  created: function() {
+    var that = this;
+     $.get(
+        "/studentView/get",
+        {}).then(function(data){
+          console.log("lastname:" + data.lastName)
+          that.studentName = data.lastName + data.firstName;
+          that.email = data.email;
+          that.perWebAddr = data.perWebAddr;
+          that.breIntr = data.breIntr;
+        });
+  },
+  methods: {
+  },
+  
+};
+</script>
