5122b7884fcbd80c4621d68f2c1e920acef65aca
卢政荣 <luzr16@mails.tsinghua.edu.cn>
2018-11-08 10:12:52 +0800

    Fix: change mysql ip & user
    change mysql ip & user

--- backend_simulator/bs_1.js
+++ backend_simulator/bs_1.js
@@ -4,9 +4,9 @@ const port = 80
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : 'b.NineEve.secoder.local',
   port     : '3306',
-  user     : 'root',
+  user     : 'lzr',
   password : 'newpass',
   database : 'A'
 });
